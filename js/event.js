3ee8b82a973ec9654b914cc28c4bd7124a4a0d3a
shircorgi <84172735+shircorgi@users.noreply.github.com>
2022-07-01 23:17:40 +0900

    Add files via upload

---
+++ js/event.js
@@ -0,0 +1,11 @@
+ //상세설명 보기
+function showDetail(){
+    document.querySelector("#desc").style.display = "block";
+    document.querySelector("#open").style.display = "none";
+}
+
+//상세설명 닫기
+function hideDetail(){
+    document.querySelector("#desc").style.display = "none";
+    document.querySelector("#open").style.display = "block";
+}
---
+++ js/img.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+
+//전역변수 선언
+var $images = null;
+function init(){
+    //배열로 이미지들을 받고 있다
+    $images = $("#container1 img");
+}
+    
+function initEvent(){
+        $("#btnStart").click(function(){
+            showImage();
+        });
+    }
+
+function showImage(){
+        //이미지 개수 구하기 
+        var length = $images.length;
+
+        //이미지 나열하기
+        for(var i=0; i<length; i++){
+            //eq() 함수를 이용해서 인덱스에 맞는 이미지를 저장
+            var $img = $images.eq(i);
+
+            //위츼의 값 구하기
+            var x = 100 + ((i % 3) * 200);
+            var y = 100 + (parseInt(i / 3) * 200);
+
+            //이미지의 위치를 설정
+            $img.css({
+                left : x, 
+                top : y
+            });
+        }
+    }    
